ffc8cf165881df28f5d38c3be614a79077345fd0
eduardosantos <eduardo.victor.017@ufrn.edu.br>
2025-05-23 14:11:54 -0300

    ajuste no utils para teste interativo

--- eyediom/utils.py
+++ eyediom/utils.py
@@ -1,10 +1,10 @@
-def scale_eye_data(eye_data, image_shape):
+def scale_eye_data(eye_data, image_shape, offset_y=0):
     """Escala as coordenadas normalizadas para dimensões reais da imagem."""
     eye_data[:, 0] *= image_shape[1]  # Largura
     eye_data[:, 1] *= image_shape[0]  # Altura
 
     #aplicando offset
-    offset_y=70
+    
     eye_data[:, 1] += offset_y
 
     return eye_data
