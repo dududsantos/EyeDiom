56e5583a289e58bc76115fe57e0c2205f1cca7bc
eduardosantos <eduardo.victor.017@ufrn.edu.br>
2025-05-23 14:13:03 -0300

    .

--- eyediom/utils.py
+++ eyediom/utils.py
@@ -1,4 +1,4 @@
-def scale_eye_data(eye_data, image_shape, offset_y=0):
+def scale_eye_data(eye_data, image_shape, offset_y):
     """Escala as coordenadas normalizadas para dimensões reais da imagem."""
     eye_data[:, 0] *= image_shape[1]  # Largura
     eye_data[:, 1] *= image_shape[0]  # Altura
