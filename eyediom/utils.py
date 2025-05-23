c2661d4e91d09fa92dc3e1e2f04d8bdb35350527
eduardosantos <eduardo.victor.017@ufrn.edu.br>
2025-05-23 14:05:19 -0300

    .

--- eyediom/utils.py
+++ eyediom/utils.py
@@ -4,7 +4,7 @@ def scale_eye_data(eye_data, image_shape):
     eye_data[:, 1] *= image_shape[0]  # Altura
 
     #aplicando offset
-    offset_y=40
+    offset_y=70
     eye_data[:, 1] += offset_y
 
     return eye_data
