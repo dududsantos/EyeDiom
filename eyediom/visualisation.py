7aae835134a0acd6058eb1e26a3868d5f51afa9c
eduardosantos <eduardo.victor.017@ufrn.edu.br>
2025-05-23 13:45:58 -0300

    Add plot_image_with_trajectory function to visualize eye trajectories on images

---
+++ eyediom/visualisation.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+def plot_image_with_trajectory(img, left_eye, right_eye):
+    """Plota a imagem com as trajetórias dos olhos sobrepostas."""
+    fig, ax = plt.subplots()
+    ax.imshow(img)
+    
+    ax.plot(left_eye[:,0], left_eye[:,1], 'b', label='Left Eye')
+    ax.plot(right_eye[:,0], right_eye[:,1], 'r', label='Right Eye')
+    
+    ax.axis('off')
+    plt.legend()
+    plt.show()
