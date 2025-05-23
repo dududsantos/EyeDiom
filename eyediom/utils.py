c168198c2a01a6bc85359a7fdf44d42b598a1a2f
eduardosantos <eduardo.victor.017@ufrn.edu.br>
2025-05-23 14:03:35 -0300

    teste de offset

--- eyediom/utils.py
+++ eyediom/utils.py
@@ -2,4 +2,9 @@ def scale_eye_data(eye_data, image_shape):
     """Escala as coordenadas normalizadas para dimensões reais da imagem."""
     eye_data[:, 0] *= image_shape[1]  # Largura
     eye_data[:, 1] *= image_shape[0]  # Altura
+
+    #aplicando offset
+    offset_y=40
+    eye_data[:, 1] += offset_y
+
     return eye_data
--- eyediom/visualization.py
+++ eyediom/visualization.py
@@ -3,8 +3,7 @@ import matplotlib.pyplot as plt
 def plot_image_with_trajectory(img, left_eye, right_eye):
     """Plota a imagem com as trajetórias dos olhos sobrepostas."""
     fig, ax = plt.subplots()
-    ax.imshow(img, extent=[0, img.shape[1], img.shape[0], 0])
-
+    ax.imshow(img)
     
     ax.plot(left_eye[:,0], left_eye[:,1], 'b', label='Left Eye')
     ax.plot(right_eye[:,0], right_eye[:,1], 'r', label='Right Eye')
