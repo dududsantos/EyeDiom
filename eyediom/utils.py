f21a01149ffe2e723c03040be830514777b5b314
eduardosantos <eduardo.victor.017@ufrn.edu.br>
2025-05-23 16:20:16 -0300

    testes de scale

--- eyediom/utils.py
+++ eyediom/utils.py
@@ -1,10 +1,20 @@
-def scale_eye_data(eye_data, image_shape, offset_y=0):
-    """Escala as coordenadas normalizadas para dimensões reais da imagem."""
-    eye_data[:, 0] *= image_shape[1]  # Largura
-    eye_data[:, 1] *= image_shape[0]  # Altura
+# Em EyeDiom/utils.py (VERSÃO FINAL SIMPLIFICADA)
 
-    #aplicando offset
-    
-    eye_data[:, 1] += offset_y
+import numpy as np
 
-    return eye_data
+def scale_eye_data(eye_data, image_shape, offset_y=0, scale_y_factor=1.0):
+   
+    scaled_data = np.copy(eye_data) # Cria uma cópia para não modificar o array original
+
+    # Escala a coordenada X apenas com a largura da imagem (assumindo que já está OK)
+    # image_shape[1] é a largura da imagem
+    scaled_data[:, 0] = scaled_data[:, 0] * image_shape[1]
+
+    # Escala a coordenada Y com a altura da imagem E o fator de escala Y
+    # image_shape[0] é a altura da imagem
+    scaled_data[:, 1] = scaled_data[:, 1] * image_shape[0] * scale_y_factor
+
+    # Aplica o offset Y
+    scaled_data[:, 1] += offset_y
+
+    return scaled_data
